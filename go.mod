76672d1fc380bdc471789a4737872285821fdfeb
Serguei Bezverkhi <sbezverk@cisco.com>
2020-11-27 12:19:35 -0500

    extend injector code to include tunnel encap tvls
    Signed-off-by: Serguei Bezverkhi <sbezverk@cisco.com>

--- go.mod
+++ go.mod
@@ -2,10 +2,12 @@ module github.com/sbezverk/srpolicy-injector
 
 go 1.15
 
-replace github.com/osrg/gobgp => ../gobgp
-
 require (
 	github.com/golang/protobuf v1.4.3
-	github.com/osrg/gobgp v0.0.0-00010101000000-000000000000
+	github.com/osrg/gobgp v0.0.0-20201125222948-c5dcfb72a847
+	github.com/sbezverk/gobgptoolbox v0.0.0-20201126160106-069765be96a1
 	google.golang.org/grpc v1.33.2
+	google.golang.org/protobuf v1.25.0
 )
+
+replace github.com/osrg/gobgp => ../gobgp
--- srpolicy-injector.go
+++ srpolicy-injector.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"encoding/binary"
 	"fmt"
 	"net"
 	"os"
@@ -9,19 +10,18 @@ import (
 	"github.com/golang/protobuf/ptypes"
 	"github.com/golang/protobuf/ptypes/any"
 	api "github.com/osrg/gobgp/api"
+	toolbox "github.com/sbezverk/gobgptoolbox"
 	"google.golang.org/grpc"
+	"google.golang.org/protobuf/types/known/anypb"
 )
 
-func AddSRPolicy() error {
-	if p == nil {
-		return fmt.Errorf("prefix is nil")
-	}
+func AddSRPolicy(client api.GobgpApiClient) error {
 
-	nlrivpn, _ := ptypes.MarshalAny(&api.LabeledVPNIPAddressPrefix{
-		Labels:    []uint32{uint32(p.Label)},
-		Rd:        p.Rd,
-		PrefixLen: p.Prefix.MaskLength,
-		Prefix:    net.IP(p.Prefix.Address).To4().String(),
+	nlrisr, _ := ptypes.MarshalAny(&api.SRPolicyNLRI{
+		Length:        96,
+		Distinguisher: 2,
+		Color:         99,
+		Endpoint:      net.ParseIP("10.0.0.15").To4(),
 	})
 	// Origin attribute
 	origin, _ := ptypes.MarshalAny(&api.OriginAttribute{
@@ -29,25 +29,81 @@ func AddSRPolicy() error {
 	})
 	// Next hop attribute
 	nh, _ := ptypes.MarshalAny(&api.NextHopAttribute{
-		NextHop: net.IP(p.NhAddress).To16().String(),
+		NextHop: net.ParseIP("192.168.20.1").To4().String(),
 	})
 	// Extended communities attribute
+	toolbox.MarshalRTFromString("")
+	rtm, err := toolbox.MarshalRTFromString("10.0.0.8:0")
+	if err != nil {
+		return err
+	}
 	rt, _ := ptypes.MarshalAny(&api.ExtendedCommunitiesAttribute{
-		Communities: p.Rt,
+		Communities: []*any.Any{rtm},
 	})
-	// Inject Prefix SID attribute
-	prefixSID, _ := ptypes.MarshalAny(&api.PrefixSID{
-		Tlvs: p.PrefixSid.Tlvs,
+	sid := make([]byte, 4)
+	binary.BigEndian.PutUint32(sid, 24321)
+	bsid, err := ptypes.MarshalAny(&api.SRBindingSID{
+		Flags: 0,
+		Sid:   sid,
 	})
-	attrs := []*any.Any{origin, nh, rt, prefixSID}
-	if _, err := bgp.client.AddPath(context.TODO(), &api.AddPathRequest{
+	if err != nil {
+		return err
+	}
+	segment, err := ptypes.MarshalAny(&api.SegmentTypeA{
+		Flags: &api.SegmentFlags{
+			SFlag: true,
+		},
+		Label: 10203,
+	})
+	if err != nil {
+		return err
+	}
+	tunTlvs, err := ptypes.MarshalAny(&api.TunnelEncapSubTLVSRPolicy{
+		Bsid:              bsid,
+		CandidatePathName: "CandidatePathName",
+		Priority:          10,
+		Enlp: &api.TunnelEncapSubTLVSRENLP{
+			Flags: 0,
+			Enlp:  api.ENLPType_Type4,
+		},
+		Preference: &api.TunnelEncapSubTLVSRPreference{
+			Flags:      0,
+			Preference: 11,
+		},
+		SegmentList: []*api.SegmentList{
+			{
+				Weight: &api.SRWeight{
+					Flags:  0,
+					Weight: 12,
+				},
+				Segments: []*any.Any{segment},
+			},
+		},
+	})
+	if err != nil {
+		return err
+	}
+	// Tunnel Encapsulation attribute for SR Policy
+	tun, err := ptypes.MarshalAny(&api.TunnelEncapAttribute{
+		Tlvs: []*api.TunnelEncapTLV{
+			{
+				Type: 15,
+				Tlvs: []*anypb.Any{tunTlvs},
+			},
+		},
+	})
+	if err != nil {
+		return err
+	}
+	attrs := []*any.Any{origin, nh, rt, tun}
+	if _, err := client.AddPath(context.TODO(), &api.AddPathRequest{
 		TableType: api.TableType_GLOBAL,
 		Path: &api.Path{
-			Nlri:      nlrivpn,
+			Nlri:      nlrisr,
 			Pattrs:    attrs,
-			Family:    &api.Family{Afi: api.Family_AFI_IP, Safi: api.Family_SAFI_MPLS_VPN},
+			Family:    &api.Family{Afi: api.Family_AFI_IP, Safi: api.Family_SAFI_SR_POLICY},
 			Best:      true,
-			SourceAsn: p.Asn,
+			SourceAsn: 65000,
 		},
 	}); err != nil {
 		return fmt.Errorf("failed to run AddPath call with error: %v", err)
@@ -57,7 +113,7 @@ func AddSRPolicy() error {
 }
 
 func main() {
-	conn, err := grpc.DialContext(context.TODO(), "192.168.20.201:40404", grpc.WithInsecure())
+	conn, err := grpc.DialContext(context.TODO(), "192.168.20.201:50051", grpc.WithInsecure())
 	if err != nil {
 		fmt.Printf("fail to connect to gobgp with error: %+v\n", err)
 		os.Exit(1)
@@ -69,7 +125,7 @@ func main() {
 		os.Exit(1)
 	}
 
-	if err := AddSRPolicy(); err != nil {
+	if err := AddSRPolicy(client); err != nil {
 		fmt.Printf("fail to add SR policy to gobgp with error: %+v\n", err)
 		os.Exit(1)
 	}
